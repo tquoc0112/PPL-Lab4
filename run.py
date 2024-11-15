eddd72179bd47d92374b5fee160e89feb2c29e85
Huỳnh Phương Đại <61302784+HarryZ2k3@users.noreply.github.com>
2024-11-15 13:18:29 +0700

    updated code to Quoc's repo

--- run.py
+++ run.py
@@ -1,87 +1,102 @@
+#PPL LAB 4 _ ITITWE21039 + ITITWE21118
 import math
-#def main(argv):
-#    print("From the input array please enter the question of which you want to see the results in")
-#    Questions(input())
 
-#def inputArray(input):
-A = []
-n = int(input("Enter number of elements of your array of numbers: (newline for each element)"))
-for i in range(0, n):
-        ele = int(input())
-        # adding the element
-        A.append(ele)  
-print("The array of number you entered:")
-print(A)
-#def Questions(input):
-#    x = input;
-#
-#    if (x == "1a"):
-#        OneA()
-#    elif (x == "1b"):
-#        OneB()
-    
+def main():
+    A = input_array()
+    while True:
+        print("\nChoose an operation to perform:")
+        print("1. (1A) Calculate squares of numbers ")
+        print("2. (1B) Calculate cubes of numbers ")
+        print("3. (1C) Filter squares in range [20, 40] ")
+        print("4. (1D) Extract even numbers ")
+        print("5. Exit")
+        
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            one_a(A)
+        elif choice == "2":
+            one_b(A)
+        elif choice == "3":
+            one_c(A)
+        elif choice == "4":
+            one_d(A)
+        elif choice == "5":
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid choice. Please try again.")
 
-#def OneA():
-    #Traditional way
-print("1A Using traditional algorithm")
-print("[", end="")
-for x in A:
-        print (x*x, end="")
+def input_array():
+    A = []
+    n = int(input("Enter the number of elements in your array of numbers: "))
+    for i in range(n):
+        ele = int(input(f"Enter element {i+1}: "))
+        A.append(ele)
+    print("The array of numbers you entered:")
+    print(A)
+    return A
+
+def one_a(A):
+    # Traditional way
+    print("1A Using traditional algorithm")
+    print("[", end="")
+    for x in A:
+        print(x * x, end="")
         if x != A[-1]:
-            print (",", end=" ")
-        
-print("]")
-#Using higher-order functions
-print ("1A Using higher-order functions")
-squared_numbers = list(map(lambda x: x * x,A))
-print(squared_numbers)
+            print(", ", end="")
+    print("]")
 
-#def OneB():
-#Traditional way
-print("1B Using traditional algorithm")
-print("[", end="")
-for x in A:
-    print (x*x*x, end="")
-    if x != A[-1]:
-        print (",", end=" ")
-        
-print("]")
+    # Using higher-order functions
+    print("1A Using higher-order functions")
+    squared_numbers = list(map(lambda x: x * x, A))
+    print(squared_numbers)
 
-    #Using higher-order functions
-print ("1B Using higher-order functions")
-threepower_numbers = list(map(lambda x: x * x * x ,A))
-print(threepower_numbers)
+def one_b(A):
+    # Traditional way
+    print("1B Using traditional algorithm")
+    print("[", end="")
+    for x in A:
+        print(x * x * x, end="")
+        if x != A[-1]:
+            print(", ", end="")
+    print("]")
 
-#def OneC():
-# 1C Using traditional algorithm
-print("1C Using traditional algorithm")
-squares_in_range = []
-for x in A:
-    square = x * x
-    if 20 <= square <= 40:
-        squares_in_range.append(square)
-print(squares_in_range)
+    # Using higher-order functions
+    print("1B Using higher-order functions")
+    cubed_numbers = list(map(lambda x: x * x * x, A))
+    print(cubed_numbers)
 
-# 1C Using higher-order functions
-print("1C Using higher-order functions")
-squares_in_range = list(map(lambda x: x if 20 <= x <= 40 else None, squared_numbers))
-squares_in_range = [x for x in squares_in_range if x is not None]
-print(squares_in_range)
+def one_c(A):
+    # 1C Using traditional algorithm
+    print("1C Using traditional algorithm")
+    squares_in_range = []
+    for x in A:
+        square = x * x
+        if 20 <= square <= 40:
+            squares_in_range.append(square)
+    print(squares_in_range)
 
-            
-#def OneD():
-#traditional way 
+    # 1C Using higher-order functions
+    print("1C Using higher-order functions")
+    squared_numbers = list(map(lambda x: x * x, A))
+    squares_in_range = list(filter(lambda x: 20 <= x <= 40, squared_numbers))
+    print(squares_in_range)
 
-print("1D Using traditional algorithm")
-print("[", end="")
-for x in A:
-    if ( x%2==0):
-        print (x, end="")
-    if x != A[-1]:
-        print (",", end=" ")
-print("]")
+def one_d(A):
+    # Traditional way
+    print("1D Using traditional algorithm")
+    print("[", end="")
+    for x in A:
+        if x % 2 == 0:
+            print(x, end="")
+        if x != A[-1]:
+            print(", ", end="")
+    print("]")
 
-#using higher-order functions
-check_round_numbers = list(map(lambda x: x if x%2 ==0 else None,A))
-round_numbers = [x for x in check_round_numbers if x is not None]
-print(round_numbers)
+    # Using higher-order functions
+    print("1D Using higher-order functions")
+    even_numbers = list(filter(lambda x: x % 2 == 0, A))
+    print(even_numbers)
+
+if __name__ == "__main__":
+    main()
